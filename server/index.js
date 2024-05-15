5a49f492000f9adf4c07fa7f5b00a6429eb7c9a0
Richard <richardlwhittington@gmail.com>
2024-05-14 21:20:23 -0500

    add server and packages

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
